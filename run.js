58bedb28ed678686e659c23a30801e4ed403acae
hazelpy <59073660+hazelpy@users.noreply.github.com>
2020-02-16 21:20:30 -0500

    Update run.js

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.use(express.static("src"));
+app.use(express.static("/public"));
 app.get("/", (req, res) => {
     if (req.get("X-Replit-User-Id")) {
         res.render("home/index.pug", {
