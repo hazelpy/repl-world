cf2c1ba1d328ad440b49749ff1cad0eb41bad4ae
hazelpy <59073660+hazelpy@users.noreply.github.com>
2020-02-16 19:58:18 -0500

    Socket on disconnect/connect function

--- src/node.js
+++ src/node.js
@@ -8,4 +8,13 @@ app.get("/", (req, res) => {
     res.sendFile("/index.html");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Listening for connections...");
+});
+
+io.on("connection", socket => {
+    console.log(`Socket with id of ${socket.id} has connected!`);
+    socket.on("disconnect", () => {
+        console.log(`Client socket id#${socket.id} has disconnected.`);
+    });
+});
