0909610648eb2489b2073d5c5c8a95e891ea9e29
hazelpy <59073660+hazelpy@users.noreply.github.com>
2020-02-16 19:38:22 -0500

    Potentially working node server with Express

---
+++ src/node.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(__dirname + "/src/public/"));
+app.get("/", (req, res) => {
+    res.sendFile("index.html");
+});
+
+app.listen(3000);
